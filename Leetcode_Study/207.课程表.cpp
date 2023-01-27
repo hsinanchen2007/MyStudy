204cb876dca09dbca0b186460be0c6d94df47b3a
Jason Chen <chen.hsinan2007@gmail.com>
2023-01-26 19:39:52 -0800

    update

---
+++ Leetcode_Study/207.课程表.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=207 lang=cpp
+ *
+ * [207] 课程表
+ */
+
+/*
+
+https://leetcode.cn/problems/course-schedule/
+
+207. 课程表
+
+中等
+1.5K
+company
+亚马逊
+company
+Facebook
+company
+谷歌 Google
+
+你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+
+在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，
+表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+
+例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+
+示例 1：
+
+输入：numCourses = 2, prerequisites = [[1,0]]
+输出：true
+解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+
+示例 2：
+
+输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+输出：false
+解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。 
+
+提示：
+
+1 <= numCourses <= 105
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+prerequisites[i] 中的所有课程对 互不相同
+
+*/
+
+// @lc code=start
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // map each course to prereq list
+        unordered_map<int, vector<int>> m;
+        for (int i = 0; i < prerequisites.size(); i++) {
+            m[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        // all courses along current DFS path
+        unordered_set<int> visited;
+        
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, m, visited)) {
+                return false;
+            }
+        }
+        return true;
+    }
+private:
+    bool dfs(int course, unordered_map<int, vector<int>>& m, unordered_set<int>& visited) {
+        if (visited.find(course) != visited.end()) {
+            return false;
+        }
+        if (m[course].empty()) {
+            return true;
+        }
+        visited.insert(course);
+        for (int i = 0; i < m[course].size(); i++) {
+            int nextCourse = m[course][i];
+            if (!dfs(nextCourse, m, visited)) {
+                return false;
+            }
+        }
+        m[course].clear();
+        visited.erase(course);
+        return true;
+    }
+};
+
+
+// @lc code=end
+
