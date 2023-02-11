d59767392510616e51c378ee974c22a70659b2f7
Jason Chen <chen.hsinan2007@gmail.com>
2023-02-11 11:27:44 -0800

    update

--- Leetcode_Study/1004.最大连续-1-的个数-iii.cpp
+++ Leetcode_Study/1004.最大连续-1-的个数-iii.cpp
@@ -296,10 +296,10 @@ public:
         int zeros = 0;
         int ans = 0;
         for (int r = 0; r < A.size(); ++r) {
-            if (A[r] == 0) ++zeros;
-            while (zeros > K)
-                if (A[l++] == 0) --zeros;
-            ans = max(ans, r - l + 1);
+        if (A[r] == 0) ++zeros;
+        while (zeros > K)
+            if (A[l++] == 0) --zeros;
+        ans = max(ans, r - l + 1);
         }
         return ans;
     }
