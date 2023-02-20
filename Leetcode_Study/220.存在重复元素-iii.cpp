873b1489dd571bd9748618243f66c2a2e88b3e33
Jason Chen <chen.hsinan2007@gmail.com>
2023-02-20 11:18:57 -0800

    update

--- Leetcode_Study/220.存在重复元素-iii.cpp
+++ Leetcode_Study/220.存在重复元素-iii.cpp
@@ -212,7 +212,7 @@ public:
 };
 
 
-class Solution {
+class Solution94 {
 public:
     // 2023.2.20, from https://github.com/grandyang/leetcode/issues/220
     /*
@@ -236,6 +236,35 @@ public:
         return false;
     }
 }; 
+
+
+class Solution {
+public:
+    // 2023.2.20, from https://zxi.mytechroad.com/blog/?s=LeetCode+220.
+    // Author: Huahua, running time: 12 ms
+    /*
+        Solution: Sliding Window + Multiset (OrderedSet)
+        Maintaining a sliding window of sorted numbers of k + 1. After the i-th number was inserted into the sliding window, 
+        check whether its left and right neighbors satisfy abs(nums[i] – neighbor) <= t
+
+        Time complexity: O(nlogk)
+        Space complexity: O(k)    
+    */
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        multiset<long> s;
+        for (int i = 0; i < nums.size(); ++i) {
+        if (i > k)
+            s.erase(s.find(nums[i - k - 1]));
+                
+        auto it = s.insert(nums[i]);
+        if (it != begin(s) && *it - *prev(it) <= t)
+            return true;
+        if (next(it) != end(s) && *next(it) - *it <= t)
+            return true;
+        }
+        return false;
+    }
+};
 
 
 // @lc code=end
