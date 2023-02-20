dcfcd462b4118a3ad6ad02e1afe20b8aab9cd99a
Jason Chen <chen.hsinan2007@gmail.com>
2023-02-20 11:20:08 -0800

    update

--- Leetcode_Study/220.存在重复元素-iii.cpp
+++ Leetcode_Study/220.存在重复元素-iii.cpp
@@ -253,14 +253,14 @@ public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         multiset<long> s;
         for (int i = 0; i < nums.size(); ++i) {
-        if (i > k)
-            s.erase(s.find(nums[i - k - 1]));
-                
-        auto it = s.insert(nums[i]);
-        if (it != begin(s) && *it - *prev(it) <= t)
-            return true;
-        if (next(it) != end(s) && *next(it) - *it <= t)
-            return true;
+            if (i > k)
+                s.erase(s.find(nums[i - k - 1]));
+                    
+            auto it = s.insert(nums[i]);
+            if (it != begin(s) && *it - *prev(it) <= t)
+                return true;
+            if (next(it) != end(s) && *next(it) - *it <= t)
+                return true;
         }
         return false;
     }
