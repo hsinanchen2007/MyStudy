f1eab91f769e7e010d21e26fb9a88fbd26805a01
Jason Chen <chen.hsinan2007@gmail.com>
2023-03-17 09:17:53 -0700

    update

--- Leetcode_Study/3.无重复字符的最长子串.cpp
+++ Leetcode_Study/3.无重复字符的最长子串.cpp
@@ -519,7 +519,7 @@ public:
 };
 
 
-class Solution {
+class Solution85 {
 public:
     // 2023.2.11, from https://zxi.mytechroad.com/blog/?s=LeetCode+3.
     // Author: Huahua
@@ -543,6 +543,32 @@ public:
             idx[s[j]] = j;
         }
         return ans;
+    }
+};
+
+
+class Solution {
+public:
+    // from educative
+    int lengthOfLongestSubstring(string s) {
+        int windowStart = 0, maxLength = 0;
+        unordered_map<char, int> charIndexMap;
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            char rightChar = s[windowEnd];
+            // if the map already contains the 'rightChar', shrink the window from the beginning so that
+            // we have only one occurrence of 'rightChar'
+            if (charIndexMap.find(rightChar) != charIndexMap.end()) {
+                // this is tricky; in the current window, we will not have any 'rightChar' after its
+                // previous index and if 'windowStart' is already ahead of the last index of 'rightChar',
+                // we'll keep 'windowStart'
+                windowStart = max(windowStart, charIndexMap[rightChar] + 1);
+            }
+            charIndexMap[rightChar] = windowEnd;  // insert the 'rightChar' into the map
+            maxLength =
+                max(maxLength, windowEnd - windowStart + 1);  // remember the maximum length so far
+        }
+        return maxLength;
     }
 };
 
