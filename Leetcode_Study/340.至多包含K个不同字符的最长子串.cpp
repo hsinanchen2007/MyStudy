7a494baddeca5e76c0d4bd2dc736b3e3ecfb5ab8
Jason Chen <chen.hsinan2007@gmail.com>
2023-03-23 08:27:52 -0700

    update

--- Leetcode_Study/340.至多包含K个不同字符的最长子串.cpp
+++ Leetcode_Study/340.至多包含K个不同字符的最长子串.cpp
@@ -354,7 +354,7 @@ public:
 /************************************************************************************************************/
 
 
-class Solution {
+class Solution88 {
 public:
     // 作者：疯狂小菜鸡
     // 链接：https://leetcode.cn/leetbook/read/sliding-window-and-two-pointers/ri87ec/?discussion=KS8hnL
@@ -378,3 +378,28 @@ public:
     }
 };
 
+
+class LongestSubstringKDistinct {
+public:
+    static int findLength(string str, int k) {
+        int windowStart = 0, maxLength = 0;
+        unordered_map<char, int> charFrequencyMap;
+        // in the following loop we'll try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+        char rightChar = str[windowEnd];
+        charFrequencyMap[rightChar]++;
+        // shrink the sliding window, until we are left with 'k' distinct characters in the frequency
+        // map
+        while ((int)charFrequencyMap.size() > k) {
+            char leftChar = str[windowStart];
+            charFrequencyMap[leftChar]--;
+            if (charFrequencyMap[leftChar] == 0) {
+                charFrequencyMap.erase(leftChar);
+            }
+            windowStart++; // shrink the window
+        }
+        maxLength = max(maxLength, windowEnd - windowStart + 1); // remember the maximum length so far
+        }
+        return maxLength;
+    }
+};
