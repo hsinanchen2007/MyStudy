c260b5572c53b5d4d21bd5895cc043253dc267ba
Jason Chen <chen.hsinan2007@gmail.com>
2023-03-22 23:07:21 -0700

    update

--- Leetcode_Study/209.长度最小的子数组.cpp
+++ Leetcode_Study/209.长度最小的子数组.cpp
@@ -513,7 +513,7 @@ public:
 };
 
 
-class Solution {
+class Solution84 {
 public:
     // 2023.2.11, from https://github.com/azl397985856/leetcode/blob/master/problems/209.minimum-size-subarray-sum.md
     /*
@@ -545,6 +545,26 @@ public:
 
 /************************************************************************************************************/
 /************************************************************************************************************/
+
+
+class Solution {
+public:
+    int minSubArrayLen(int S, vector<int>& arr) {
+        int minLength = numeric_limits<int>::max();
+        int windowSum = 0;
+        int windowStart = 0;
+        for (int windowEnd = 0; windowEnd < arr.size(); windowEnd++) {
+            windowSum += arr[windowEnd];
+            // shrink the window as small as possible until the 'windowSum' is smaller than 'S'
+            while (windowSum >= S) {
+                minLength = min(minLength, windowEnd - windowStart + 1);
+                windowSum -= arr[windowStart];
+                windowStart++;
+            }      
+        }
+        return minLength == numeric_limits<int>::max() ? 0 : minLength;
+    }
+};
 
 
 // @lc code=end
