50e5bca80c927799c4f1bdd5e949890dad8fd2de
Jason Chen <chen.hsinan2007@gmail.com>
2023-03-22 22:21:15 -0700

    update

--- Leetcode_Study/53.最大子数组和.cpp
+++ Leetcode_Study/53.最大子数组和.cpp
@@ -397,10 +397,10 @@ public:
         vector<int> dp(nums.size());
 
         for (int i = 0; i < nums.size(); ++i)
-        if (i > 0 && dp[i - 1] > 0)
-            dp[i] = dp[i - 1] + nums[i];
-        else
-            dp[i] = nums[i];
+            if (i > 0 && dp[i - 1] > 0)
+                dp[i] = dp[i - 1] + nums[i];
+            else
+                dp[i] = nums[i];
 
         return *max_element(begin(dp), end(dp));
     }
