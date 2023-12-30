414701000e423f47cc98953f05ed65baaa3bfe15
Hsinan Chen <chen.hsinan2007@gmail.com>
2023-12-29 22:39:22 -0800

    update

---
+++ Leetcode_2024/1185.day-of-the-week.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=1185 lang=cpp
+ * @lcpr version=30112
+ *
+ * [1185] 一周中的第几天
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 31\n8\n2019\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 18\n7\n1999\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 15\n8\n1993\n
+// @lcpr case=end
+
+ */
+
